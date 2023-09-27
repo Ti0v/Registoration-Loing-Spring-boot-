8c644d77b5953ff8d0acd1117f3ccde8ac81cfb3
Ti0v <100233720+Ti0v@users.noreply.github.com>
2023-09-27 19:20:48 +0300

    mazefn

--- src/main/java/com/ThymeleafExample/Thymeleaf/Security/SpringSecurityConfig.java
+++ src/main/java/com/ThymeleafExample/Thymeleaf/Security/SpringSecurityConfig.java
@@ -1,2 +1,26 @@
-package com.ThymeleafExample.Thymeleaf.Security;public class SpringSecurityConfig {
+package com.ThymeleafExample.Thymeleaf.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SpringSecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public UserDetailsService service(){
+        UserDetails mazen =  User.builder().username("Mazen")
+                .password(passwordEncoder().encode("Na1133nn.")).build();
+
+       return new InMemoryUserDetailsManager(mazen);
+    }
+
 }
